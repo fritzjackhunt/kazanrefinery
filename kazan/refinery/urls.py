0eda1d54a05fb52f4b2bb8ac6b0a2393787eb279
Aniekan Essiet <essiet.aniekan@yahoo.com>
2023-12-21 04:22:28 +0300

    About us page

--- kazan/refinery/urls.py
+++ kazan/refinery/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage'),
+    path('about', views.about, name='about'),
 ]
--- kazan/refinery/views.py
+++ kazan/refinery/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'kazan/index.html')
+
+def about(request):
+    return render(request, 'kazan/about.html')
